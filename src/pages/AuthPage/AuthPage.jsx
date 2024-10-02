3f087d22af6c8c34174b9fc8c62904c00f99588a
SenithNimsara <senith2002n@gmail.com>
2024-10-02 19:04:59 +0530

    create AuthForms

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -1,15 +1,29 @@
-import { Container, Flex } from '@chakra-ui/react'
+import { Box, Container, Flex, Image, Stack, VStack, } from '@chakra-ui/react'
 import React from 'react'
+import AuthForm from '../../components/AuthForm/AuthForm';
 
 const AuthPage = () => {
   return (
     <Flex minH={"100vh"} justifyContent={"center"} alignItems={"center"} px={4}>
         <Container maxW={"container.md"} padding={0}>
-            this is some content
+          <Flex justifyContent={"center"} align={"center"}>
+            {/* Left hand -side */}
+            <Box display={{base:"none",md:"block"}}>
+              <Image src="/auth.png" h={650} alt='phone img' />
+              </Box>
+            {/* Right hand -side */}
+            <VStack spacing={4} align={"stretch"}>
+              <AuthForm />
+              <Box textAlign={"center"}>Get the app</Box>
+              <Flex gap={5} justifyContent={"center"}>
+                <Image src='/playstore.png' h={"10"} alt='Playstore logo' onClick={"pointer"} />
+                <Image src='/playstore.png' h={"10"} alt='Playstore logo' onClick={"pointer"} />
+              </Flex>
+            </VStack >
+          </Flex>
         </Container>
-        
-    </Flex>
-  )
-}
+     </Flex>
+  );
+};
 
 export default AuthPage
